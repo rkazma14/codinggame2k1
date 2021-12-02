3ac34a10016255e5f066acc9a6cd83213b41b06c
Thibaud ANDREVON <thibaud.andrevon@gmail.com>
2021-12-02 17:28:23 +0100

    Fixes

--- main.py
+++ main.py
@@ -2,14 +2,29 @@ import sys
 import random
 from operator import attrgetter
 
-bonus_ranking = {"POINTS_3": 7,\
-                "POINTS_2": 6, \
-                "ENERGY_CORE": 5,\
-                "POINTS_1": 4,\
-                "ALIEN_ARTIFACT": 3,\
-                "TECH_RESEARCH_2": 2,\
-                "TECH_RESEARCH_3": 1,\
-                "TECH_RESEARCH_4": 0}
+bonus_ranking_by_points = {
+    "POINTS_3": 7,
+    "POINTS_2": 6, 
+    "ENERGY_CORE": 1,
+    "POINTS_1": 5,
+    "ALIEN_ARTIFACT": 0,
+    "TECH_RESEARCH_2": 4,
+    "TECH_RESEARCH_3": 3,
+    "TECH_RESEARCH_4": 2
+}
+
+bonus_ranking_by_tech = {
+    "POINTS_3": 7,
+    "POINTS_2": 6, 
+    "ENERGY_CORE": 4,
+    "POINTS_1": 5,
+    "ALIEN_ARTIFACT": 3,
+    "TECH_RESEARCH_2": 10,
+    "TECH_RESEARCH_3": 9,
+    "TECH_RESEARCH_4": 8
+}
+
+number_of_objectives_reached_before_we_target_point = 1
 
 class Game:
     def __init__(self):
@@ -27,6 +42,17 @@ class Game:
     def reset(self):
         self.current_station_index = -1
         self.my_stations.sort(key=attrgetter('objective_score'), reverse=True)
+
+    def get_objectives_reached(self):
+        count = 0
+        for station in self.my_stations:
+            reached = True
+            for i in range(len(station.tech)):
+                if (station.tech[i] < self.get_station_objective_by_id(station.id).tech_objectives[i]):
+                    reached = False
+            if reached:
+                count += 1
+        return count
 
     def get_station_objective_by_id(self, station_id):
         return self.station_objectives[str(station_id)]
@@ -123,11 +149,17 @@ class Game:
             return self.get_tech_research_command_line(4)
         return None
 
-    def get_best_preferred_bonus(self, planet):
-        if bonus_ranking[planet.bonuses[0]] > bonus_ranking[planet.bonuses[1]]:
-            return 0
+    def get_best_preferred_bonus(self, planet, objectives_reached):
+        if (objectives_reached > number_of_objectives_reached_before_we_target_point):
+            if bonus_ranking_by_points[planet.bonuses[0]] > bonus_ranking_by_points[planet.bonuses[1]]:
+                return 0
+            else:
+                return 1
         else:
-            return 1
+            if bonus_ranking_by_tech[planet.bonuses[0]] > bonus_ranking_by_tech[planet.bonuses[1]]:
+                return 0
+            else:
+                return 1
 
 
     def get_action(self):
@@ -150,12 +182,12 @@ class Game:
             for station in self.my_stations:
                 if station.available:
                     for planet in self.planets:
-                        combos.append(Combo(station, planet))
+                        combos.append(Combo(station, planet, self.get_objectives_reached()))
             combos.sort(key=attrgetter('planet_bonus_score', 'score', 'planet.colonization_score', 'planet_tasks_sum'), reverse=True)
 
             for combo in combos:
                 if self.should_colonize_planet(combo.planet, combo.station):
-                    bonus = self.get_best_preferred_bonus(combo.planet)
+                    bonus = self.get_best_preferred_bonus(combo.planet, self.get_objectives_reached())
                     return "COLONIZE {0} {1} {2}".format(combo.station.id, combo.planet.id, bonus)
             return 'RESUPPLY'
 
@@ -186,21 +218,27 @@ class Planet:
     def tasks_remaining(self):
         return self.tasks[0] + self.tasks[1] + self.tasks[2] + self.tasks[3]
 
-    def bonus_score(self):
-        if bonus_ranking[self.bonuses[0]] > bonus_ranking[self.bonuses[1]]:
-            return bonus_ranking[self.bonuses[0]]
+    def bonus_score(self, objectives_reached):
+        if (objectives_reached > number_of_objectives_reached_before_we_target_point):
+            if bonus_ranking_by_points[self.bonuses[0]] > bonus_ranking_by_points[self.bonuses[1]]:
+                return bonus_ranking_by_points[self.bonuses[0]]
+            else:
+                return bonus_ranking_by_points[self.bonuses[1]]
         else:
-            return bonus_ranking[self.bonuses[1]]
+            if bonus_ranking_by_tech[self.bonuses[0]] > bonus_ranking_by_tech[self.bonuses[1]]:
+                return bonus_ranking_by_tech[self.bonuses[0]]
+            else:
+                return bonus_ranking_by_tech[self.bonuses[1]]
 
 class Combo:
-    def __init__(self, station, planet):
+    def __init__(self, station, planet, objectives_reached):
         self.id = id
         self.station = station
         self.planet = planet
         self.score = 0
         self.set_score()
         self.planet_tasks_sum = 16 - self.planet.tasks_remaining()
-        self.planet_bonus_score = self.planet.bonus_score()
+        self.planet_bonus_score = self.planet.bonus_score(objectives_reached)
 
     def set_score(self):
         self.score = 0
